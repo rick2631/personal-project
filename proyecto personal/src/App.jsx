69d4ce08193f9a85daba5930d1c78b427b37f561
Rick Zamora <dtvv90@datamatrixcompany.com>
2024-07-22 00:39:29 -0500

    Tu mensaje descriptivo del commit

---
+++ proyecto personal/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <section className="section section-1">
+        <h1>Presentacion1</h1>
+        <p>Contenido de la sección 1</p>
+      </section>
+      <section className="section section-2">
+        <h1>About me 2</h1>
+        <p>Contenido de la sección 2</p>
+      </section>
+      <section className="section section-3">
+        <h1>Projects 3</h1>
+        <p>Contenido de la sección 3</p>
+      </section>
+    </div>
+  );
+};
+
+export default App;
